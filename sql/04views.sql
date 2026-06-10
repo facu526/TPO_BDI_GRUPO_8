d4646d4c8e3d881a93a8e31e0c51fa8571aea937
Santiago Tomas Peralta <sanperalta@uade.edu.ar>
2026-06-10 20:29:10 -0300

    Update 04views.sql

--- sql/04views.sql
+++ sql/04views.sql
@@ -1,1 +1,69 @@
+USE ComplejoDeportivo;
+GO
 
+
+CREATE VIEW VW_Reservas_Cliente_Estado AS
+SELECT 
+    r.id_reserva,
+    c.id_cliente,
+    c.nombre + ' ' + c.apellido AS cliente,
+    c.dni,
+    er.nombre AS estado_reserva,
+    r.fecha,
+    r.hora_inicio,
+    r.hora_fin,
+    r.monto_total
+FROM Reserva r
+JOIN Cliente c ON r.id_cliente = c.id_cliente
+JOIN Estado_Reserva er ON r.id_estado_reserva = er.id_estado_reserva;
+GO
+
+
+CREATE VIEW VW_Canchas_Sede_Deporte AS
+SELECT 
+    ca.id_cancha,
+    ca.nro_cancha,
+    ca.nombre AS cancha,
+    ca.estado,
+    ca.precio_por_hora,
+    s.nombre AS sede,
+    s.direccion AS direccion_sede,
+    d.nombre AS deporte
+FROM Cancha ca
+JOIN Sede s ON ca.id_sede = s.id_sede
+JOIN Deporte d ON ca.id_deporte = d.id_deporte;
+GO
+
+
+CREATE VIEW VW_Pagos_Metodo_Estado AS
+SELECT 
+    p.id_pago,
+    p.id_reserva,
+    mp.nombre AS metodo_pago,
+    ep.nombre AS estado_pago,
+    p.monto,
+    p.fecha_hora
+FROM Pago p
+JOIN Metodo_Pago mp ON p.id_metodo_pago = mp.id_metodo_pago
+JOIN Estado_Pago ep ON p.id_estado_pago = ep.id_estado_pago;
+GO
+
+CREATE VIEW VW_Promociones_Vigentes AS
+SELECT
+    p.id_promocion,
+    p.nombre AS promocion,
+    p.descuento_porcentaje,
+    p.fecha_inicio,
+    p.fecha_fin,
+    d.nombre AS deporte,
+    s.nombre AS sede
+FROM Promocion p
+LEFT JOIN Deporte d ON p.id_deporte = d.id_deporte
+LEFT JOIN Sede s ON p.id_sede = s.id_sede
+WHERE GETDATE() BETWEEN p.fecha_inicio AND p.fecha_fin;
+GO
+
+SELECT * FROM VW_Reservas_Cliente_Estado;
+SELECT * FROM VW_Canchas_Sede_Deporte;
+SELECT * FROM VW_Pagos_Metodo_Estado;
+SELECT * FROM VW_Promociones_Vigentes;
